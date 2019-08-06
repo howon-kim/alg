0bf12d45cd156e20b7f8b7e380dd5e2432f8b08c
Howon Kim <howonkim@me.com>
2019-08-05 17:36:46 -0700

    08/05/2019

---
+++ Leetcode/21. Merge Two Sorted Lists.java
@@ -0,0 +1,102 @@
+/* -----------------------------------
+21. Merge Two Sorted Lists | Easy
+https://leetcode.com/problems/merge-two-sorted-lists/
+
+ *  My Submission Performance
+Runtime: 0 ms, faster than 100.00% of Java online submissions for Merge Two Sorted Lists.
+Memory Usage: 40.1 MB, less than 14.15% of Java online submissions for Merge Two Sorted Lists.
+
+* Description
+Merge two sorted linked lists and return it as a new list. The new list should be made by splicing together the nodes of the first two lists.
+
+* Example
+Input: 1->2->4, 1->3->4
+Output: 1->1->2->3->4->4
+ ----------------------------------- */
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+
+
+class Solution {
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        if (l1 == null) {
+            return l2;
+        } else if (l2 == null) {
+            return l1;
+        } else if (l1.val < l2.val) {
+            l1.next = mergeTwoLists(l1.next, l2);
+            return l1;
+        } else {
+            l2.next = mergeTwoLists(l1, l2.next);
+            return l2;
+        }
+    }
+}
+
+public class MainClass {
+    public static int[] stringToIntegerArray(String input) {
+        input = input.trim();
+        input = input.substring(1, input.length() - 1);
+        if (input.length() == 0) {
+          return new int[0];
+        }
+    
+        String[] parts = input.split(",");
+        int[] output = new int[parts.length];
+        for(int index = 0; index < parts.length; index++) {
+            String part = parts[index].trim();
+            output[index] = Integer.parseInt(part);
+        }
+        return output;
+    }
+    
+    public static ListNode stringToListNode(String input) {
+        // Generate array from the input
+        int[] nodeValues = stringToIntegerArray(input);
+    
+        // Now convert that list into linked list
+        ListNode dummyRoot = new ListNode(0);
+        ListNode ptr = dummyRoot;
+        for(int item : nodeValues) {
+            ptr.next = new ListNode(item);
+            ptr = ptr.next;
+        }
+        return dummyRoot.next;
+    }
+    
+    public static String listNodeToString(ListNode node) {
+        if (node == null) {
+            return "[]";
+        }
+    
+        String result = "";
+        while (node != null) {
+            result += Integer.toString(node.val) + ", ";
+            node = node.next;
+        }
+        return "[" + result.substring(0, result.length() - 2) + "]";
+    }
+    
+    public static void main(String[] args) throws IOException {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        String line;
+        while ((line = in.readLine()) != null) {
+            ListNode l1 = stringToListNode(line);
+            line = in.readLine();
+            ListNode l2 = stringToListNode(line);
+            
+            ListNode ret = new Solution().mergeTwoLists(l1, l2);
+            
+            String out = listNodeToString(ret);
+            
+            System.out.print(out);
+        }
+    }
+}
